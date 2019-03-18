088491e659cfddd9f8b35e3539b9706e4df2b306
codejockey02 <codejockey02@gmail.com>
2019-03-19 03:38:29 +0530

    Linked with firebase

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ const app = express();
 const bodyParser = require('body-parser');
 const api = require('./api.js');
 const SendOtp = require('sendotp');
+const admin = require('firebase-admin');
 const sendOtp = new SendOtp('242758AH31FYTYG5bc3238e');
+const serviceAccount = require('./serviceAccountKey.json');
 const port = process.env.PORT || 3000;
 app.use(bodyParser.json({
     limit: '25mb',
@@ -12,11 +14,15 @@ bodyParser.urlencoded({
     extended: true,
     limit: '25mb',
 });
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://trackme-2b844.firebaseio.com/",
+});
+
 var voiceDeliveryReceiptApi = new api.VoiceApi("priyeshs2", "54AAA751-70D1-6183-BAB5-6492CAEAB8F5");
-var smsApi = new api.SMSApi("priyeshs2", "54AAA751-70D1-6183-BAB5-6492CAEAB8F5");
 
 var voiceMessage = new api.VoiceMessage();
-var smsMessage = new api.SmsMessage();
 
 app.post('/call', async (req, res) => {
     const number = req.body.number;
@@ -57,8 +63,17 @@ app.post('/allowaccess', async (req, res) => {
 });
 
 app.post('/verify', async (req, res) => {
+    const pnumber = req.body.parent;
+    const number = req.body.child;
+    const name = req.body.childname;
     const otp = req.body.otp;
     if (otp == "569812") {
+        const link = `/Users/${pnumber}/allowed/${number}/`;
+        let ref = admin.database().ref(link);
+        const obj = {
+            name,
+        };
+        ref.set(obj);
         res.json({
             error: false,
             code: null,
